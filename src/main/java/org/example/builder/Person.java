b8511584368e7887528788efdbf0be6676092a5a
Alexander Biezbardis <mr.biezbardis@gmail.com>
2021-07-29 20:32:24 +0300

    Added realization

--- src/main/java/org/example/builder/Person.java
+++ src/main/java/org/example/builder/Person.java
@@ -1,5 +1,7 @@
 package org.example.builder;
 
+import java.util.Objects;
+
 public class Person {
     protected final String name;
     protected final String surname;
@@ -49,6 +51,14 @@ public class Person {
         this.age++;
     }
 
+    public PersonBuilder newChildBuilder() {
+        PersonBuilder builder = new PersonBuilder()
+                .setSurname(this.surname)
+                .setAge(0)
+                .setAddress(this.address);
+        return builder;
+    }
+
     @Override
     public String toString() {
         return "Name: \t" + this.name +
@@ -58,10 +68,16 @@ public class Person {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return name.equals(person.name) &&
+                surname.equals(person.surname);
+    }
+
+    @Override
     public int hashCode() {
-        return (this.name +
-                this.surname +
-                this.age +
-                this.address).hashCode();
+        return Objects.hash(name, surname);
     }
 }
--- src/main/java/org/example/builder/PersonBuilder.java
+++ src/main/java/org/example/builder/PersonBuilder.java
@@ -8,33 +8,49 @@ public class PersonBuilder {
     private Person person;
 
     public PersonBuilder setName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
         this.name = name;
         return this;
     }
 
     public PersonBuilder setSurname(String surname) {
+        if (surname.isEmpty()) {
+            throw new IllegalArgumentException("Фамилия не может быть пустой");
+        }
         this.surname = surname;
         return this;
     }
 
     public PersonBuilder setAge(int age) {
+        if (age < 0 || age > 200) {
+            throw new IllegalArgumentException("Возраст может принимать значения от 0 до 200");
+        }
         this.age = age;
         return this;
     }
 
     public PersonBuilder setAddress(String address) {
+        if (surname.isEmpty()) {
+            throw new IllegalArgumentException("Адрес не может быть пустым");
+        }
         this.address = address;
         return this;
     }
 
     public Person build() {
-        if (this.age < 0) {
-            person = new Person(name, surname);
-        } else {
-            person = new Person(name, surname, age);
-        }
-        if (!address.equals("")) {
-            person.setAddress(this.address);
+        try {
+            if (this.age < 0) {
+                person = new Person(name, surname);
+            } else {
+                person = new Person(name, surname, age);
+            }
+            if (!address.isEmpty() || address != null) {
+                person.setAddress(this.address);
+            }
+        } catch (IllegalStateException e) {
+            System.out.println("Недостаточно данных для создания записи");
         }
         return person;
     }
